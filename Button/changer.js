3f666381d35f01103d3784e74c426d07b2617fec
alecvd <avandijken.de@mysummitps.org>
2015-10-01 21:32:55 +0000

    [message about changes]

---
+++ Button/changer.js
@@ -0,0 +1,10 @@
+// JavaScript File
+var btn1 = document.getElementById("button1")
+var inpt1 = document.getElementById("input1")
+var header1 = document.getElementById("theHeader")
+
+btn1.addEventListener("click",function(){
+    var newColor = inpt1.value;
+    header1.style.color = newColor;
+    btn1.style.-color = newColor;
+})
